13a78e775531234ac53f3eba793c4ef149228c85
Hans Christian Nenseth <nenseth@gmail.com>
2021-05-17 21:18:32 +0200

    Oslo as map center

--- app/src/main/java/tech/darksun/ridemyscooter/MapsActivity.kt
+++ app/src/main/java/tech/darksun/ridemyscooter/MapsActivity.kt
@@ -41,8 +41,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val oslo = LatLng(60.0, 10.0)
+        mMap.addMarker(MarkerOptions().position(oslo).title("Marker in Oslo"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(oslo))
     }
 }
